b5e8bf01af9260842a8c25f764a4206c69ae3136
unknown <tylerstanczak@gmail.com>
2023-10-06 12:11:45 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Stack, TextArea, RadioButton, RadioButtonGroup, Button, Form, Grid, Column } from '@carbon/react';
+import { CustomHeader } from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <CustomHeader />
+      <Grid fullWidth style={{ marginTop: '300px' }}>
+        <Column lg={{ start: 7, end: 11 }} md={{ start: 4, end: 7 }} sm={{ start: 2 }}>
+          <Form aria-label="">
+            <Stack gap={5}>
+              <TextArea labelText="Text Area label" rows={2} id="text-area-1" />
+
+              <RadioButtonGroup name="radio-button-group" defaultSelected="Greedy">
+                <RadioButton value="Greedy" id="radio-1" labelText="Greedy" />
+                <RadioButton value="Sample" labelText="Sample" id="radio-2" />
+              </RadioButtonGroup>
+
+              <Button>Submit</Button>
+            </Stack>
+          </Form>
+        </Column>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import {
+    Header,
+    HeaderName,
+    HeaderGlobalBar,
+    HeaderGlobalAction,
+    HeaderSideNavItems
+} from "@carbon/react";
+import { Search, Notification, Switcher as SwitcherIcon } from '@carbon/icons-react';
+export const CustomHeader = () => {
+    return (
+        <Header aria-label="IBM WatsonX">
+            <HeaderSideNavItems>asdf</HeaderSideNavItems>
+            <HeaderName href="#" prefix="IBM">
+                WatsonX
+            </HeaderName>
+            <HeaderGlobalBar>
+                <HeaderGlobalAction aria-label="Search" >
+                    <Search size={20} />
+                </HeaderGlobalAction>
+                <HeaderGlobalAction aria-label="Notifications">
+                    <Notification size={20} />
+                </HeaderGlobalAction>
+                <HeaderGlobalAction tooltipAlignment="end" id="switcher-button">
+                    <SwitcherIcon size={20} />
+                </HeaderGlobalAction>
+            </HeaderGlobalBar>
+        </Header>
+    )
+}
